5ecd3e31584b2a32fddf5264fd76a009bb946954
Bernardo Duarte <bernardoduartes@gmail.com>
2019-01-21 21:27:27 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,40 @@
+package com.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimanto;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimanto, Date dataPagamento ) {
+		super(id, estado, pedido);
+		this.dataVencimanto = dataVencimanto;
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataVencimanto() {
+		return dataVencimanto;
+	}
+
+	public void setDataVencimanto(Date dataVencimanto) {
+		this.dataVencimanto = dataVencimanto;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+}
